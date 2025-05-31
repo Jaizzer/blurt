c584a021b2fe08fc12c26c472c53fb389c743976
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-05-31 13:31:29 +0800

    chore: Setup postgres db connection pool

---
+++ models/pool.js
@@ -0,0 +1,31 @@
+const { Pool } = require('pg');
+
+let pool;
+try {
+    const connectionString =
+        process.argv[2] === "PRODUCTION"
+            ? process.env.PRODUCTION_DB_URL
+            : process.env.LOCAL_DB_URL;
+
+    if (!connectionString) {
+        throw new Error(
+            "Database connection string not found in environment variables.",
+        );
+    }
+
+    pool = new Pool({
+        connectionString: connectionString,
+        ssl:
+            process.argv[2] === "PRODUCTION"
+                ? {
+                    rejectUnauthorized: false,
+                }
+                : false,
+    });
+    console.log("Database pool created successfully.");
+} catch (error) {
+    console.error("Error creating a database pool. ", error);
+    process.exit(1);
+}
+
+module.exports = pool;
