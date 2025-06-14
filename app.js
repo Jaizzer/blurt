50f7d8f487a43bcd5e8fc3648e0af3dd81487fe6
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-06-14 21:04:38 +0800

    style: Format code

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 // Load the file path management module
-const path = require('path');
+const path = require("path");
 
 // Load the environment variables
 const dotenv = require("dotenv");
 dotenv.config();
 
 // Load the dependencies for handling multipart/form data
-const multer = require('multer');
+const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
 // Load ejs layout
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 
 // Setup the server
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, (error) => {
-    if (error) {
-        console.error(`Failed to start server: ${error}`);
-    } else {
-        const baseUrl =
-            process.argv[2] === "PRODUCTION"
-                ? process.env.PRODUCTION_URL
-                : `http://localhost:${PORT}`;
-        console.log(`Server is listening on: ${baseUrl}`);
-    }
+	if (error) {
+		console.error(`Failed to start server: ${error}`);
+	} else {
+		const baseUrl =
+			process.argv[2] === "PRODUCTION"
+				? process.env.PRODUCTION_URL
+				: `http://localhost:${PORT}`;
+		console.log(`Server is listening on: ${baseUrl}`);
+	}
 });
 
 // Serve public files
@@ -38,26 +38,24 @@ app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-
 // Root router
 const rootRouter = require("./routes/rootRouter");
 app.use("/", rootRouter);
 
-
 // Main error-handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    const statusCode = err.statusCode || err.status || 500;
-    const message = err.message || "Internal server error";
-    console.log(message);
-    res.status(statusCode).render("error", {
-        title: "404 Error",
-        message: "Internal Server Error",
-    });
+	console.error(err.stack);
+	const statusCode = err.statusCode || err.status || 500;
+	const message = err.message || "Internal server error";
+	console.log(message);
+	res.status(statusCode).render("error", {
+		title: "404 Error",
+		message: "Internal Server Error",
+	});
 });
 
 // Error handling for uncaught exceptions
 process.on("uncaughtException", (error) => {
-    console.error(`Uncaught exception: ${error}`);
-    process.exit(1);
+	console.error(`Uncaught exception: ${error}`);
+	process.exit(1);
 });
