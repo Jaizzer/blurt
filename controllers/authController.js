b69568449140ad8b94af5a136102d2c465a13292
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-06-17 06:26:29 +0800

    feat: Create function that gets all the form field data including the previous values and errors in each input field

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,6 +64,31 @@ async function signInGet(req, res, next) {
 	});
 }
 
+function getFormFieldData(req) {
+	// Get the input errors
+	const inputErrors = validationResult(req).mapped();
+
+	// Create an object that contains each input field's error and value.
+	const formFieldData = {};
+	const inputFieldNames = Object.keys(req.body);
+	inputFieldNames.forEach((inputFieldName) => {
+		const value = inputErrors[inputFieldName]
+			? inputErrors[inputFieldName].value
+			: req.body[inputFieldName];
+
+		const error = inputErrors[inputFieldName]
+			? inputErrors[inputFieldName].msg
+			: null;
+
+		formFieldData[inputFieldName] = {
+			value,
+			error,
+		};
+	});
+
+	return formFieldData;
+}
+
 const validateSignUpForm = [
 	body("email")
 		.trim()
