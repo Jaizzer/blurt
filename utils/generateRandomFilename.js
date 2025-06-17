d4f6273d92d69cdf9ac820b79774c884af46dbe0
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-06-18 07:33:09 +0800

    ref: Rename module for more generalize use

--- utils/generateRandomFilename.js
+++
@@ -1,3 +0,0 @@
-const crypto = require('crypto');
-const generateRandomFilename = (bytes = 32) => crypto.randomBytes(bytes).toString("hex");
-module.exports = { generateRandomFilename };
---
+++ utils/generateRandomString.js
@@ -0,0 +1,3 @@
+const crypto = require('crypto');
+const generateRandomString = (bytes = 32) => crypto.randomBytes(bytes).toString("hex");
+module.exports = { generateRandomString };
