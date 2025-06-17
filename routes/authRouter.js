bfb28a93bd03d79eab3b92d6eede0c17e42f4457
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-06-17 15:39:57 +0800

    feat: Use authValidators and authMiddlewares in /signIn route for input validation and error handling

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const authRouter = Router();
 const authController = require("../controllers/authController.js");
+const authValidators = require("../validators/authValidators.js");
+const authMiddlewares = require("../middlewares/authMiddlewares.js");
 
 authRouter.get("/signUp", authController.signUpGet);
 authRouter.post("/signUp", authController.signUpPost);
 authRouter.get("/signIn", authController.signInGet);
+authRouter.post("/signIn", authValidators.signIn, authMiddlewares.validateSignInForm, authController.signInPost);
 
 module.exports = authRouter;
