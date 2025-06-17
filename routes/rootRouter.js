653b00025f564cc1d69393d3d130b669d68acfd9
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-06-18 07:04:58 +0800

    feat: Create initial use case for the authentication middleware

--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const rootRouter = Router();
 const rootControllers = require("../controllers/rootControllers");
+const authMiddlewares = require("../middlewares/authMiddlewares.js");
 
-rootRouter.get("/", rootControllers.rootGet);
+rootRouter.get("/", authMiddlewares.isAuthenticated, rootControllers.rootGet);
 
 module.exports = rootRouter;
