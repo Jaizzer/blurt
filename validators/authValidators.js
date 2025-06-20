1d67834563d03f583a7277ac8b23b2d59466c7ae
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-06-20 20:31:51 +0800

    feat: Ensure the email and username are unique to each user

--- validators/authValidators.js
+++ validators/authValidators.js
@@ -1,4 +1,5 @@
 const { body } = require("express-validator");
+const User = require("../models/userModel.js");
 
 const signUp = [
 	body("email")
@@ -6,14 +7,28 @@ const signUp = [
 		.notEmpty()
 		.withMessage("Please provide an email.")
 		.isEmail()
-		.withMessage("Please provide a valid email."),
+		.withMessage("Please provide a valid email.")
+		.custom(async (value, { req }) => {
+			const isEmailAlreadyTaken = await User.getByEmail(value);
+			if (isEmailAlreadyTaken) {
+				throw new Error("Email is already taken");
+			}
+			return true;
+		}),
 
 	body("username")
 		.trim()
 		.notEmpty()
 		.withMessage("Please provide a username")
 		.isAlphanumeric()
-		.withMessage("Please provide alphanumeric username."),
+		.withMessage("Please provide alphanumeric username.")
+		.custom(async (value, { req }) => {
+			const isUsernameAlreadyTaken = await User.getByUsername(value);
+			if (isUsernameAlreadyTaken) {
+				throw new Error("Username is already taken");
+			}
+			return true;
+		}),
 
 	body("password").trim().notEmpty().withMessage("Please provide a password"),
 
