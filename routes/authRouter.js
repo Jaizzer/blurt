119e57c55b14bb3c27902ee50972c2932a28ee41
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-06-21 11:34:41 +0800

    ref: Format code

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,12 +5,26 @@ const authValidators = require("../validators/authValidators.js");
 const authMiddlewares = require("../middlewares/authMiddlewares.js");
 
 authRouter.get("/signUp", authControllers.signUpGet);
-authRouter.post("/signUp", authValidators.signUp, authMiddlewares.validateSignUpForm, authControllers.signUpPost);
+authRouter.post(
+	"/signUp",
+	authValidators.signUp,
+	authMiddlewares.validateSignUpForm,
+	authControllers.signUpPost
+);
 authRouter.get("/signIn", authControllers.signInGet);
-authRouter.post("/signIn", authValidators.signIn, authMiddlewares.validateSignInForm, authControllers.signInPost);
+authRouter.post(
+	"/signIn",
+	authValidators.signIn,
+	authMiddlewares.validateSignInForm,
+	authControllers.signInPost
+);
 authRouter.get("/verify/:emailVerificationString", authControllers.verifyUser);
-authRouter.get("/signOut", authMiddlewares.isAuthenticated, authControllers.signOut);
+authRouter.get(
+	"/signOut",
+	authMiddlewares.isAuthenticated,
+	authControllers.signOut
+);
 authRouter.get("/google", authControllers.initializeSignInWithGoogle);
-authRouter.get("/google/callback", authControllers.signInWithGoogle );
+authRouter.get("/google/callback", authControllers.signInWithGoogle);
 
 module.exports = authRouter;
