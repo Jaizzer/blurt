76be0f6d9d26363971a0a148b3c1b1461b1406b5
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-06-22 11:37:07 +0800

    feat: Create model for the linked_accounts table

---
+++ models/linkedAccountModel.js
@@ -0,0 +1,81 @@
+const pool = require("../config/pool.js");
+
+async function add({ provider, providerUserId, userId }) {
+	try {
+		const { rows } = await pool.query(
+			`
+            INSERT INTO linked_accounts
+            (provider, provider_user_id, user_id)
+            VALUES ($1, $2, $3)
+            RETURNING *;
+        `,
+			[provider, providerUserId, userId]
+		);
+		return rows[0];
+	} catch (error) {
+		console.error("Error inserting linked account.");
+		throw error;
+	}
+}
+
+async function getById(id) {
+	try {
+		const { rows } = await pool.query(
+			`
+            SELECT *
+            FROM linked_accounts
+            WHERE id = $1;
+            `,
+			[id]
+		);
+		const linkedAccount = rows[0];
+		return linkedAccount;
+	} catch (error) {
+		console.error("Error retrieving linked account");
+		throw error;
+	}
+}
+
+async function getByForeignKeyUserId(id) {
+	try {
+		const { rows } = await pool.query(
+			`
+            SELECT *
+            FROM linked_accounts
+            WHERE user_id = $1;
+            `,
+			[id]
+		);
+		const linkedAccount = rows[0];
+		return linkedAccount;
+	} catch (error) {
+		console.error("Error retrieving linked account");
+		throw error;
+	}
+}
+
+async function getByProviderAndProviderUserId({ provider, providerUserId }) {
+	try {
+		const { rows } = await pool.query(
+			`
+            SELECT *
+            FROM linked_accounts
+            WHERE provider = $1
+            AND provider_user_id = $2;
+            `,
+			[provider, providerUserId]
+		);
+		const linkedAccount = rows[0];
+		return linkedAccount;
+	} catch (error) {
+		console.error("Error retrieving linked account");
+		throw error;
+	}
+}
+
+module.exports = {
+	add,
+	getById,
+	getByForeignKeyUserId,
+	getByProviderAndProviderUserId,
+};
